2fca4f362f9e286d12d72fdfdd43a95ff5de5549
k <nomerdvadcatpyat@gmail.com>
2021-04-19 22:59:50 +0500

    uptade webpack + change files structure

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -1,9 +1,3 @@
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const paths = require('./paths');
 
 const babelLoader = {
@@ -19,22 +13,6 @@ const babelLoader = {
 module.exports = {
   entry: `${paths.src}/index.js`,
   mode: 'none',
-  output: {
-    path: paths.build,
-    filename: 'bundle.js',
-    publicPath: paths.public,
-    clean: true,
-    crossOriginLoading: 'anonymous',
-    module: true,
-    environment: {
-      arrowFunction: true,
-      bigIntLiteral: false,
-      const: true,
-      destructuring: true,
-      dynamicImport: false,
-      forOf: true
-    }
-  },
   resolve: {
     alias: {
       '@': `${paths.src}/modules`
@@ -47,46 +25,19 @@ module.exports = {
   },
   module: {
     rules: [
-      // JavaScript
       {
         test: /\.js$/i,
         exclude: /\*\/node_modules/,
         use: babelLoader
       },
-      // CSS, SASS
       {
         test: /\.(c|sa|sc)ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: ['css-loader']
       },
-      // static files
       {
         test: /\.(jpe?g|png|gif|svg|eot|ttf|woff2?)$/i,
         type: 'asset'
       }
-    ]
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-
-    new MiniCssExtractPlugin({
-      filename: 'bundle.css'
-    }),
-
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: `${paths.public}`
-        }
-      ]
-    }),
-    new CleanWebpackPlugin()
-  ],
-
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new UglifyJsPlugin()
     ]
   }
 };
