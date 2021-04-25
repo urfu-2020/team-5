05c052fb97a664f6e1929e081ae47ed19096229c
k <nomerdvadcatpyat@gmail.com>
2021-04-25 12:30:43 +0500

    add ripple effect

--- client/src/components/Controls/Button/Button.jsx
+++ client/src/components/Controls/Button/Button.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './button.css';
+import { Ripple } from './Ripple/Ripple';
 
 export const Button = ({children, ...otherProps}) => {
 
   return (
     <button {...otherProps}>
       {children}
+      <Ripple duration={800} />
     </button>
   );
 };
