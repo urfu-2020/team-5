dcc99e7027b1c54f5b0b61da4b022cda0699beb3
Pavel <nomerdvadcatpyat@gmail.com>
2021-05-16 02:54:00 +0500

    vipilil socket io

--- client/src/store/middlewares/socketMiddleware.js
+++ client/src/store/middlewares/socketMiddleware.js
@@ -1,4 +1,3 @@
-import { io } from "socket.io-client";
 import {addChatMessage} from "../slices/chatsSlice/chatsSlice";
 import {setCurrentUser} from "../slices/userSlice/userThunks";
 import {setChatsData} from "../slices/chatsSlice/chatsThunks";
@@ -9,31 +8,42 @@ export const sendMessage = payload => ({type: SEND_MESSAGE, payload});
 
 let socket;
 
-const initSocket = (store) => {
-  socket = io();
-  socket.on('chatMessage', message => {
-    store.dispatch({ type: addChatMessage.type, payload: message});
-  });
+const initSocket = (store, userId) => {
+  socket = new WebSocket(process.env.REACT_APP_BACKEND_WEBSOCKET_URL);
+
+  socket.onopen = function () {
+    socket.send(JSON.stringify({ type: 'setUserId', payload: userId }));
+  };
+
+  socket.onmessage = function(event) {
+    const message = JSON.parse(event.data);
+    switch (message.type) {
+      case 'chatMessage': {
+        store.dispatch({ type: addChatMessage.type, payload: message.payload});
+        return;
+      }
+    }
+  };
 };
 
 export const socketMiddleware = store => next => action => {
   switch (action.type) {
     case setCurrentUser.fulfilled.type: {
-      // socket = new WebSocket(process.env.REACT_APP_BACKEND_WEBSOCKET_URL);
       if(action.payload) {
-        initSocket(store);
+        const {id} = action.payload;
+        initSocket(store, id);
       }
       return next(action);
     }
 
     case setChatsData.fulfilled.type: {
       const chatIds = Object.keys(action.payload);
-      socket.emit('setChats', chatIds);
+      socket.send(JSON.stringify({ type: 'setChats', payload: chatIds}));
       return next(action);
     }
 
     case SEND_MESSAGE: {
-      socket.emit('chatMessage', action.payload);
+      socket.send(JSON.stringify({ type: 'chatMessage', payload: action.payload }));
       return;
     }
 
