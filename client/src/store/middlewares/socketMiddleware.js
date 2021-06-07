b7d23cdc1ee62bc58daedc1ed753af5f367b2804
k <nomerdvadcatpyat@gmail.com>
2021-06-07 20:09:10 +0500

    socket reconnect

--- client/src/store/middlewares/socketMiddleware.js
+++ client/src/store/middlewares/socketMiddleware.js
@@ -15,54 +15,75 @@ import {
 
 let socket;
 
+function connect(store) {
+  socket = new WebSocket(process.env.REACT_APP_BACKEND_WEBSOCKET_URL);
+  if(store.getState().app.error) {
+    store.dispatch(setError(null));
+  }
+  socket.onmessage = function (event) {
+    const message = JSON.parse(event.data);
+    const {payload} = message;
+    switch (message.type) {
+      case 'ping': {
+        socket.send(JSON.stringify({type: 'pong'}));
+        break;
+      }
+      case 'setChatsData': {
+        store.dispatch(setChatsData(payload));
+        break;
+      }
+      case 'setTheme': {
+        store.dispatch(setStartTheme(payload));
+        break;
+      }
+      case 'addNewChat': {
+        store.dispatch(addNewChat(payload));
+        break;
+      }
+      case 'subscribeToChannel': {
+        store.dispatch(addNewChat(payload));
+        store.dispatch(setIsSubscribing(false));
+        break;
+      }
+      case 'unsubscribeFromChannel': {
+        store.dispatch(leaveFromChat(payload));
+        store.dispatch(setIsSubscribing(false));
+        break;
+      }
+      case 'chatMessage': {
+        store.dispatch(addChatMessage(payload));
+        break;
+      }
+      case 'errorMessage': {
+        store.dispatch(setError(payload));
+        break;
+      }
+      case 'setUnsubscribedChannel': {
+        store.dispatch(setCurrentUnsubscribeChannel(payload));
+        break;
+      }
+    }
+  };
+
+  socket.onclose = function() {
+    store.dispatch(setError({ error: true,
+      errorMessage: "Соеденение прервано. Повторное подключение..."}));
+    setTimeout(function() {
+      connect();
+    }, 1000);
+  };
+
+  socket.onerror = function() {
+    store.dispatch(setError({ error: true,
+      errorMessage: "Ошибка соеденения. Перезагрузите страницу."}));
+    socket.close();
+  };
+}
+
 export const socketMiddleware = store => next => action => {
   switch (action.type) {
     case INIT_SOCKET: {
-      socket = new WebSocket(process.env.REACT_APP_BACKEND_WEBSOCKET_URL);
-      socket.onmessage = function (event) {
-        const message = JSON.parse(event.data);
-        const {payload} = message;
-        switch (message.type) {
-          case 'ping': {
-            socket.send(JSON.stringify({type: 'pong'}));
-            break;
-          }
-          case 'setChatsData': {
-            store.dispatch(setChatsData(payload));
-            break;
-          }
-          case 'setTheme': {
-            store.dispatch(setStartTheme(payload));
-            break;
-          }
-          case 'addNewChat': {
-            store.dispatch(addNewChat(payload));
-            break;
-          }
-          case 'subscribeToChannel': {
-            store.dispatch(addNewChat(payload));
-            store.dispatch(setIsSubscribing(false));
-            break;
-          }
-          case 'unsubscribeFromChannel': {
-            store.dispatch(leaveFromChat(payload));
-            store.dispatch(setIsSubscribing(false));
-            break;
-          }
-          case 'chatMessage': {
-            store.dispatch(addChatMessage(payload));
-            break;
-          }
-          case 'errorMessage': {
-            store.dispatch(setError(payload));
-            break;
-          }
-          case 'setUnsubscribedChannel': {
-            store.dispatch(setCurrentUnsubscribeChannel(payload));
-            break;
-          }
-        }
-      };
+      connect(store);
       break;
     }
 
